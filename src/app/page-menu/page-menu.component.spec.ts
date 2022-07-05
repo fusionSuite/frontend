b27baa47b589bcc0c0f172e0622e7ec29ad10a66
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-07-05 09:42:09 +0200

    Fix tests

--- src/app/page-menu/page-menu.component.spec.ts
+++ src/app/page-menu/page-menu.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PageMenuComponent } from './page-menu.component';
 
@@ -9,6 +11,10 @@ describe('PageMenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PageMenuComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
     })
       .compileComponents();
 
