f4cd24ccfb6656642f7ada3f5870a93df24eb904
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-08-29 17:25:08 +0200

    imp: Handle errors at the initialization
    Before, when an error occured during the initialization of the
    application, nothing was shown.
    
    Now, errors raised during the bootstrap of the application (i.e.
    `APP_INITIALIZER` provider) are catched and displayed to the user.
    
    This change also display a loader while the application is not fully
    loaded.

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,25 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(error => {
+  const loadingElement = document.querySelector('#app-loading') as HTMLElement;
+  if (loadingElement) {
+    loadingElement.hidden = true;
+  }
+
+  const errorElement = document.querySelector('#app-error') as HTMLElement;
+
+  let message = 'Application initialization failed';
+  if (error && error.message) {
+    message = message + ': ' + error.message;
+  } else if (error) {
+    message = message + ': ' + error;
+  }
+
+  if (errorElement) {
+    errorElement.textContent = message;
+    errorElement.hidden = false;
+  }
+
+  console.error(message);
+});
